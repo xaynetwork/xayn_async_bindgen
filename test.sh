c42bf425c27728948a2ff3d31716f8ba4b10ea90
Philipp Korber <philipp@korber.dev>
2021-12-14 16:24:38 +0100

    Updated test.sh.

--- test.sh
+++ test.sh
@@ -9,5 +9,8 @@ cargo clippy --all
 cargo test
 
 cargo build -p integration-tests-bindings
+cargo run -p async-bindgen-gen-dart -- \
+    --ffi-class IntegrationTestsFfi \
+    --genesis integration_tests/lib/src/genesis.ffigen.dart
 cd integration_tests
 dart test
