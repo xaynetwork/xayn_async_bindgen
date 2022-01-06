9c893caeb1966c1ef72b3ea141090a9368a2f311
Philipp Korber <philipp@korber.dev>
2022-01-06 08:00:20 +0100

    Added cargo sort.

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 set -eu
 cd "$(dirname $0)"
 
+cargo +nightly fmt --all -- --check
+cargo sort --grouped --workspace --check
+
+cargo clippy --all
+
 cargo test
 
 cargo build -p integration-tests-bindings
